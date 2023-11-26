9900c89daf60d4707ce14ccbe41ff3258d0950d3
Viacheslav <vacheslavuslistyj@gmail.com>
2023-11-26 11:29:31 +0200

    installed allure and added labels to the tests

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Selenium4XPytest
-Selenium4 in combination with Pytest example
+# Selenium4 X Pytest X Allure
+Selenium4 in combination with Pytest test framework, and allure retorting example.
+
+
+## Setup for Windows
+
+### Install Allure 
+1. Download Java 8+ : https://www.oracle.com/java/technologies/downloads/
+2. Set `JAVA_HOME` environment variable, point it to the java root folder.
+3. Download and unpack latest allure release: https://github.com/allure-framework/allure2/releases
+   4. Or using Scoop: https://allurereport.org/docs/gettingstarted-installation/#install-via-scoop-for-windows
+5. Into `PATH` - add path to the allure.bat file. Ex: `*\allure\allure-2.24.1\bin`
--- main_test.py
+++ main_test.py
@@ -1,8 +1,16 @@
-from time import sleep
+import allure
 
 from ui_comp import SearchBarComponent, CoockiesModalComponent, VideoSearchListComponent
 
 
+# @allure.title("Test Authentication")
+@allure.description("Verification that search request present in the 1st shown video name.")
+@allure.tag("Searchbar")
+@allure.severity(allure.severity_level.BLOCKER)
+@allure.label("owner", "Viacheslav Uslistyi")
+@allure.label("task", "SOME_ID_1")
+# @allure.link("https://dev.example.com/", name="Website")
+# @allure.testcase("TMS-456")
 def test_video_search(selenium):
     selenium.get('https://www.youtube.com/')
     coockies_modal = CoockiesModalComponent(selenium)
@@ -15,7 +23,14 @@ def test_video_search(selenium):
     videos = video_list.get_video_list()
     assert 'test' in videos[0].name.lower()
 
-
+# @allure.title("Test Authentication")
+@allure.description("Verification that search terms remains in input after a search.")
+@allure.tag("Searchbar")
+@allure.severity(allure.severity_level.NORMAL)
+@allure.label("owner", "Viacheslav Uslistyi")
+@allure.label("task", "SOME_ID_2")
+# @allure.link("https://dev.example.com/", name="Website")
+# @allure.testcase("TMS-456")
 def test_search_value_remains_in_input(selenium):
     selenium.get('https://www.youtube.com/')
     coockies_modal = CoockiesModalComponent(selenium)
